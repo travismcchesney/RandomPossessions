51e6aa27e215cd2bf4cc5e66d13593e1dfd4869f
Travis McChesney <travisj37@hotmail.com>
2012-04-26 21:01:13 -0600

    Added bug by accessing 11th item in array

--- RandomPossessions/main.m
+++ RandomPossessions/main.m
@@ -26,6 +26,8 @@ int main(int argc, const char * argv[])
             NSLog(@"%@", item);
         }
         
+        BNRItem *item = [items objectAtIndex:10];
+        
         items = nil;
     }
     return 0;
