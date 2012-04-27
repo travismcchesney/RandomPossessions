ff9d511276cc422cc10e170596fb7e17c3d9a245
Travis McChesney <travisj37@hotmail.com>
2012-04-26 20:52:47 -0600

    End of chapter 2

--- RandomPossessions/main.m
+++ RandomPossessions/main.m
@@ -8,14 +8,25 @@
 
 #import <Foundation/Foundation.h>
 
+#import "BNRItem.h"
+
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
+
+        NSMutableArray *items = [[NSMutableArray alloc] init];
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        for (int i = 0; i < 10; i++) {
+            BNRItem *p = [BNRItem randomItem];
+            [items addObject:p];
+        }
         
+        for (BNRItem *item in items) {
+            NSLog(@"%@", item);
+        }
+        
+        items = nil;
     }
     return 0;
 }
