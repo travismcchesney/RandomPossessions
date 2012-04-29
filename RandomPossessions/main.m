9415ac40fd509593324196241f3e7bc0f8e960c6
Travis McChesney <travisj37@hotmail.com>
2012-04-28 21:14:36 -0600

    Before starting properties lesson

--- RandomPossessions/main.m
+++ RandomPossessions/main.m
@@ -14,21 +14,20 @@ int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
-
-        NSMutableArray *items = [[NSMutableArray alloc] init];
         
         BNRItem *backpack = [[BNRItem alloc] init];
         [backpack setItemName:@"Backpack"];
-        [items addObject:backpack];
         
         BNRItem *calculator = [[BNRItem alloc] init];
         [calculator setItemName:@"Calculator"];
-        [items addObject:calculator];
         
         [backpack setContainedItem:calculator];
+
+        backpack = nil;
         
-        NSLog(@"Setting items to nil...");
-        items = nil;
+        NSLog(@"Container: %@", [calculator container]);
+        
+        calculator = nil;
     }
     return 0;
 }
